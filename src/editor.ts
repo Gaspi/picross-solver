38e776b20f361daaa3029cb06b05971c092ef8cb
Gaspard FEREY <gaspard.ferey@insee.fr>
2026-01-02 15:14:51 +0100

    Editor as ts as well

--- src/editor.ts
+++ src/editor.ts
@@ -1,9 +1,25 @@
 import { get, mk, wipe  } from './utils.js';
 
-function spec(row) {
+class Cell { color=0; }
+
+class Picross {
+  rows: Cell[][];
+  cols: Cell[][];
+  constructor(height: number, width: number) {
+    this.rows = new Array(height).fill(0).map(   () => new Array(width ).fill(0).map(   () => new Cell()     ));
+    this.cols = new Array(width ).fill(0).map((_,i) => new Array(height).fill(0).map((_,j) => this.rows[j][i]));
+  }
+}
+
+var picross: Picross;
+var rows: HTMLElement[], cols: HTMLElement[], cells: HTMLElement[][];
+
+var dimX: HTMLElement;
+
+function spec(row: Cell[]): number[] {
   const res = new Array();
   let acc = 0;
-  row.forEach(function(c) {
+  row.forEach(function(c: Cell) {
     if (c.color === 1) {
       acc += 1
     } else {
@@ -15,18 +31,8 @@ function spec(row) {
   return res;
 }
 
-class Cell { color=0; }
 
-class Picross {
-  constructor(height, width) {
-    this.rows = new Array(height).fill(0).map(   () => new Array(width ).fill(0).map(   () => new Cell()     ));
-    this.cols = new Array(width ).fill(0).map((_,i) => new Array(height).fill(0).map((_,j) => this.rows[j][i]));
-  }
-}
-
-var cells, rows, cols, picross;
-
-function setColor(i,j,c) {
+function setColor(i: number, j: number, c: number) {
   picross.rows[i][j].color = c;
   if (c) {
     cells[i][j].classList.replace('white','black');
@@ -37,7 +43,7 @@ function setColor(i,j,c) {
   cols[j].innerHTML = spec( picross.cols[j] ).join('<br>');
 }
 
-function mkCell(i,j) {
+function mkCell(i: number, j: number) {
   const c = cells[i][j] = mk('td', ['pic-cell','white']);
   c.addEventListener("click", function () {
     setColor(i,j,1);
@@ -50,13 +56,13 @@ function mkCell(i,j) {
   return c;
 }
 
-function initPicross(height, width) {
+function initPicross(height: number, width: number) {
   picross = new Picross(height, width);
   rows = new Array(height);
   cols = new Array(width);
   cells = new Array(height).fill(0).map(()=>new Array(width));
 
-  const pic = wipe( get('picross') );
+  const pic = wipe( get('picross') as HTMLElement );
   const specRow = pic.appendChild( mk('tr',['pic-row']) );
   specRow.appendChild( mk('th',['pic-corner']) );
   for (let j = 0; j < width; j++) {
@@ -69,26 +75,29 @@ function initPicross(height, width) {
       row.appendChild( mkCell(i,j) );
     }
   }
-  get('copy').disabled = false;
-  get('copyspec').disabled = false;
+  (get('copy'    ) as HTMLButtonElement).disabled = false;
+  (get('copyspec') as HTMLButtonElement).disabled = false;
 }
 
 function newPicross() {
-  initPicross(parseInt(get('dim-x').innerText) , parseInt(get('dim-y').innerText));
+  initPicross(
+    parseInt( (get('dim-x') as HTMLElement).innerText ) ,
+    parseInt( (get('dim-y') as HTMLElement).innerText )
+  );
 }
 
-function loadPicrossFromString(txt) {
+function loadPicrossFromString(txt: string) {
   const spec = JSON.parse(txt);
   initPicross(spec.height, spec.width);
-  spec.rows.forEach(function (row, i) {
-    row.forEach(function (c, j) {
+  spec.rows.forEach(function (row: number[], i: number) {
+    row.forEach(function (c: number, j: number) {
       setColor(i, j, c);
     });
   });
 }
 
-function copyObject(o) {
-  navigator.clipboard.writeText(JSON.stringify(o)).then(alert("Copied"));
+function copyObject(o: any) {
+  navigator.clipboard.writeText(JSON.stringify(o)).then(()=>{alert("Copied");});
 }
 
 function copyPicross() {
@@ -105,17 +114,17 @@ function pastePicross() {
 
 function copySpec() {
   const txt = `copied;${picross.rows.map((x)=>spec(x).join('.')).join(',')};${picross.cols.map((x)=>spec(x).join('.')).join(',')}`;
-  navigator.clipboard.writeText(txt).then(alert("Copied"));
+  navigator.clipboard.writeText(txt).then(()=>{alert("Copied");});
 }
 
 window.onload = function() {
-  get("newpicross").addEventListener("click", newPicross);
-  get("copy").addEventListener("click", copyPicross);
-  get("paste").addEventListener("click", pastePicross);
-  get("copyspec").addEventListener("click", copySpec);
+  (get("newpicross") as HTMLElement).addEventListener("click", newPicross);
+  (get("copy"      ) as HTMLElement).addEventListener("click", copyPicross);
+  (get("paste"     ) as HTMLElement).addEventListener("click", pastePicross);
+  (get("copyspec"  ) as HTMLElement).addEventListener("click", copySpec);
 
-  const field_x = get("field-x");
-  field_x.addEventListener("change", function () { get('dim-x').innerText = field_x.value; });
-  const field_y = get("field-y");
-  field_y.addEventListener("change", function () { get('dim-y').innerText = field_y.value; });
+  const field_x = get("field-x") as HTMLInputElement;
+  field_x.addEventListener("change", function () { (get("field-x") as HTMLElement).innerText = field_x.value; });
+  const field_y = get("field-y") as HTMLInputElement;
+  field_y.addEventListener("change", function () { (get('dim-y') as HTMLElement).innerText = field_y.value; });
 }
